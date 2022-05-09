ce9f84dc5f70593f4ae6c1393c4e3eb7d66814b7
Morgan Lyle <morganlylee@gmail.com>
2022-05-09 17:14:36 -0500

    Feature 4 complete

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from projects.models import (
+    Project,
+)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Project, ProjectAdmin)
