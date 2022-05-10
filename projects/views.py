985a60c838f7ee53396bada63255a83296bbf37c
Morgan Lyle <morganlylee@gmail.com>
2022-05-09 19:49:34 -0500

    Feature 8, test not finding project name in list, no projects have been made?

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
 # from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.views.generic.list import ListView
 
 from projects.models import Project
@@ -6,6 +8,9 @@ from projects.models import Project
 # Create your views here.
 
 
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/list.html"
+
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
