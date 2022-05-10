69e20647e8c680762cb84df6389e29f638b0a327
Morgan Lyle <morganlylee@gmail.com>
2022-05-09 20:33:25 -0500

    Feature 10 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "account Created successfully")
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/login.html", {"form": form})
