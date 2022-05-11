901910b7e5fbfa1e903e2ab6664efd2980a11b72
Morgan Lyle <morganlylee@gmail.com>
2022-05-10 20:10:54 -0500

    Feature 15, 1 error will go back

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.urls import reverse_lazy
+
+from django.views.generic.edit import CreateView
+from tasks.models import Task
 
 # Create your views here.
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = "tasks/create.html"
+    fields = ["name", "start_date", "due_date", "project", "assignee"]
+    successful_url = reverse_lazy("home")
